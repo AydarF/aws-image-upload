02ec6bdcf18808f290e61e37056c653f8eb0de79
Aydar Fayzullin <faidarik@mail.ru>
2021-10-08 22:14:10 -0400

    First commit

---
+++ src/main/java/com/aydarfz/awsimageupload/config/AmazonConfig.java
@@ -0,0 +1,32 @@
+package com.aydarfz.awsimageupload.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Service;
+
+@Configuration
+public class AmazonConfig {
+    @Value("${aws.awsAccessKey}") // Access hidden awsAccessKey
+    private String awsAccessKey;
+    @Value("${aws.awsSecretKey}") // Access hidden awsSecretKey
+    private String awsSecretKey;
+
+    @Bean
+    public AmazonS3 s3(){
+
+        AWSCredentials awsCredentials = new BasicAWSCredentials(
+                this.awsAccessKey,
+                this.awsSecretKey
+        );
+        return AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
+    }
+}
