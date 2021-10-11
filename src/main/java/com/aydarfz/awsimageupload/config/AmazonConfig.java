d054173aed50bf88cfa3c0737c82dbc116834273
Aydar Fayzullin <faidarik@mail.ru>
2021-10-11 03:25:20 -0400

    Implement API and service layers

--- src/main/java/com/aydarfz/awsimageupload/config/AmazonConfig.java
+++ src/main/java/com/aydarfz/awsimageupload/config/AmazonConfig.java
@@ -26,6 +26,7 @@ public class AmazonConfig {
         );
         return AmazonS3ClientBuilder
                 .standard()
+                .withRegion("us-east-1")
                 .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
                 .build();
     }
---
+++ src/test/java/com/aydarfz/awsimageupload/MainEntryPointTest.java
@@ -0,0 +1,7 @@
+package com.aydarfz.awsimageupload;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainEntryPointTest {
+
+}
