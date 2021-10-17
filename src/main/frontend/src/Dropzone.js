8e4fe706ca1a8e4adba3d959597750b3d9aa93f7
Aydar Fayzullin <faidarik@mail.ru>
2021-10-17 00:24:16 -0400

    Create UI logic to send files to backend

--- src/main/frontend/src/Dropzone.js
+++ src/main/frontend/src/Dropzone.js
@@ -1,10 +1,29 @@
+import axios from 'axios';
 import {useCallback} from 'react'
 import {useDropzone} from 'react-dropzone'
 
-function Dropzone() {
+function Dropzone({userProfileId}) {
   const onDrop = useCallback(acceptedFiles => {
     const file = acceptedFiles[0];
     console.log(file);
+    // Send files to backend
+    const formData = new FormData();
+    // Add file to formData
+    // "file" has to be exactly the same as in UserProfileController.java class
+    formData.append("file", file);
+    // Post file
+    axios.post(`http://localhost:8080/api/v1/user-profile/${userProfileId}/image/upload`,
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data"
+        }
+      }
+    ).then(() => {
+      console.log("File uploaded successfully");
+    }).catch(err => {
+      console.log(err);
+    });
   }, [])
   const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
 
